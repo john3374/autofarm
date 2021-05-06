59750be34e9d0926cd4666e71f39905018ad4175
John Lee <john3374@gmail.com>
2021-05-05 22:22:19 -0400

    added dotenv package

--- mysqlPool.js
+++ mysqlPool.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
    connectionLimit: 6,
-   host: "localhost",
-   user: "farmer",
-   password: "W3m7SuYaGIt3",
-   database: "autofarm"
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_SCHEMA
 });
 const x = {pool: pool, escape: mysql.escape};
 module.exports = x;
